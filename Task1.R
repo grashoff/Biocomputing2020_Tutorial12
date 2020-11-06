9584076d13aaf93c5bb2de4b6cd13c2c37be81de
grashoff <grashoff@nd.edu>
2020-11-05 22:08:18 -0500

    Added comments on coding methods for the first task

--- Task1.R
+++ Task1.R
@@ -1,8 +1,13 @@
+# load the libraries necessary to produce the plot
 library(ggplot2)
 library(cowplot)
+
+#read in the excel file with the dataset and confirm it was loaded correctly
 dat = read.csv("sbry.csv", header = TRUE)
 head(dat)
 dim(dat)
+
+#use the data set "dat" from the excel file; set the x and y variables, chart them as a scatterplot, label the axes, and then add a trendline
 ggplot(data = dat,
        aes(x = Precipitation, y = Strawberry.Yield)) +
   geom_point() +
